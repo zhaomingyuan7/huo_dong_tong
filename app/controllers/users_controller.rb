fc43abb06847dd85d5f205bf7b2105f8b847f8a8
zhaomingyuan7 <zhaomingyuan7@gmail.com>
2014-06-04 17:26:04 +0800

    solve can not save register

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,30 +2,31 @@
 class UsersController < ApplicationController
   def login
   end
+
   def welcome
   end
+
   def post
   end
+
   def register
     @user = User.new
   end
+
   def create
     params.permit!
     @user = User.new(params[:user])
     if @user.save
-      #cookies.permanent[:token]=@user.token
+      cookies.permanent[:token]=@user.token
       redirect_to :'welcome'
     else
       render :register
     end
   end
+
   def create_login_session
     user = User.find_by_name(params[:name])
-    p ''''''''''''''
-    p user
-    p ''''''''''''''
-    if user
-      cookies.permanent[:token] = user.token
+    if user && user.authenticate(params[:password_digest])
       redirect_to :welcome
     else
       flash[:error]='用户名不存在或密码错误'
